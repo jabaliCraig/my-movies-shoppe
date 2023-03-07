2b6d2b0bf9fc1d6dcb6d5968d28b4416394d2741
Craig <71907929+jabaliCraig@users.noreply.github.com>
2023-03-06 19:14:16 -0700

    Third time's a charm... eventually

--- server/db/_db.js
+++ server/db/_db.js
@@ -9,15 +9,15 @@ const config = {
 if (process.env.LOGGING === "true") {
   delete config.logging;
 }
+if (process.env.DATABASE_URL) {
+	config.dialectOptions = {
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	};
+}
 const db = new Sequelize(
   process.env.DATABASE_URL,
   config
 );
 module.exports = db;
-if (process.env.DATABASE_URL) {
-  config.dialectOptions = {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  };
-}
