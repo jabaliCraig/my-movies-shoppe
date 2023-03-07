cface7a8b42ea8eea61f4af16ca3ec182107e0bf
Craig <71907929+jabaliCraig@users.noreply.github.com>
2023-03-06 18:57:49 -0700

    I'm trusting you, Jon

--- server/db/_db.js
+++ server/db/_db.js
@@ -1,13 +1,23 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
+const pkg = require("../../package.json");
 require("dotenv").config();
+const databaseName =
+  pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 const config = {
   logging: false,
-}
- if(process.env.LOGGING){
+};
+if (process.env.LOGGING === "true") {
   delete config.logging;
- }
-
-
-const db = new Sequelize(process.env.DATABASE_URL ||'postgres://craig:tVkAfiLkOO5Bi1mJ1HWzMstmWrmIcnd6@dpg-cg38cs5269v3bp90c43g-a/grace_cinema_l074?ssl=true', config);
-
+}
+const db = new Sequelize(
+  process.env.DATABASE_URL,
+  config
+);
 module.exports = db;
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
