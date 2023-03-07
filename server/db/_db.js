0267e19d98f8d32707072b0c62e69f1f391b90fd
Craig <71907929+jabaliCraig@users.noreply.github.com>
2023-03-06 17:59:38 -0700

    reverting to external db link

--- server/db/_db.js
+++ server/db/_db.js
@@ -8,6 +8,6 @@ const config = {
  }
 
 
-const db = new Sequelize(process.env.DATABASE_URL ||'postgres://craig:tVkAfiLkOO5Bi1mJ1HWzMstmWrmIcnd6@dpg-cg38cs5269v3bp90c43g-a/grace_cinema_l074', config);
+const db = new Sequelize(process.env.DATABASE_URL ||'postgres://craig:tVkAfiLkOO5Bi1mJ1HWzMstmWrmIcnd6@dpg-cg38cs5269v3bp90c43g-a.oregon-postgres.render.com/grace_cinema_l074?ssl=true', config);
 
 module.exports = db;
